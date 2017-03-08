622809c1e6ccdd998b301e069ffb063b454dfce8
Mert Kilic <mert.kilic@icterra.com>
2017-03-09 00:06:12 +0300

    Timeout limit decreased

--- app/src/main/java/com/mertkilic/countries/util/Constants.java
+++ app/src/main/java/com/mertkilic/countries/util/Constants.java
@@ -10,7 +10,7 @@ public class Constants {
 
     public static final String BASE_URL = "https://restcountries.eu/rest/v2/";
 
-    public static final int NETWORK_CONNECTION_TIMEOUT = 30; // 30 sec
+    public static final int NETWORK_CONNECTION_TIMEOUT = 10; // 10 sec
     public static final long CACHE_SIZE = 5 * 1024 * 1024; // 5 MB
     public static final int CACHE_MAX_STALE = 7; // 7 day
 }
